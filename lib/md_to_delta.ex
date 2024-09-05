493096caae22e7a82fe19026768aa123fd5a2ffc
John Joseph Ugalino <john@lawadvisor.com>
2024-09-05 16:35:03 +0800

    Add library skeleton

---
+++ lib/md_to_delta.ex
@@ -0,0 +1,18 @@
+defmodule MdToDelta do
+  @moduledoc """
+  Documentation for `MdToDelta`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MdToDelta.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/md_to_delta_test.exs
@@ -0,0 +1,8 @@
+defmodule MdToDeltaTest do
+  use ExUnit.Case
+  doctest MdToDelta
+
+  test "greets the world" do
+    assert MdToDelta.hello() == :world
+  end
+end
