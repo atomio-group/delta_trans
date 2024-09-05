ec9cd1b3159cb5ecc5cfae0fec3e1015cb805d63
richardpitargue <richard.pitargue@lawadvisor.com>
2024-09-05 17:07:46 +0800

    Add tests

--- test/md_to_delta_test.exs
+++ test/md_to_delta_test.exs
@@ -2,7 +2,45 @@ defmodule MdToDeltaTest do
   use ExUnit.Case
   doctest MdToDelta
 
-  test "greets the world" do
-    assert MdToDelta.hello() == :world
+  @fixtures_dir "test/fixtures"
+  @expected_dir "test/expected"
+
+  test_types = File.ls!(@fixtures_dir)
+
+  for type <- test_types do
+    describe "#{type} parsing" do
+      test_dir = @fixtures_dir <> "/#{type}"
+
+      test_file_names =
+        test_dir
+        |> File.ls!()
+        |> Enum.map(&String.replace(&1, ".md", ""))
+        |> Enum.sort()
+
+      for file_name <- test_file_names do
+        test file_name do
+          file_name = unquote(file_name)
+          type = unquote(type)
+
+          file_path = "/#{type}/" <> file_name
+
+          expected =
+            @expected_dir
+            |> Kernel.<>(file_path)
+            |> Kernel.<>(".json")
+            |> File.read!()
+            |> Jason.decode!()
+
+          actual =
+            @fixtures_dir
+            |> Kernel.<>(file_path)
+            |> Kernel.<>(".md")
+            |> File.read!()
+            |> MdToDelta.parse()
+
+          assert actual == expected
+        end
+      end
+    end
   end
 end
